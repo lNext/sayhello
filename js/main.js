a9fac129fdb5eb88256533e2d4443eb93a31ec24
Sebastián <sebastian.contactoweb@gmail.com>
2022-07-04 15:37:35 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 function nombreOnClick(){
     let name = document.getElementById("name").value;
-    /* let message = 'Hi ' + name; */
-
     if (name == ''){
         alert('Please enter your name.')
     }else{
         document.getElementById("greetings").innerHTML = 'Hi ' + name + ', great to see you!';
+        document.getElementById("name").value = '';
     }
 }
